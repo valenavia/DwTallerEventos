de7605c35c6d3be3aaa6565ea319ba4dfd30cac8
Vale <133821850+valenavia@users.noreply.github.com>
2023-08-10 13:39:19 -0300

    corrección botón

--- eventos.js
+++ eventos.js
@@ -1,8 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
 const div = document.querySelector("div");
-
+const boton = document.querySelector("button");
+    
 function saludar(){
     alert("Hola! Soy el div");
+}
+
+boton.addEventListener("click", function(event) {
+event.stopPropagation();
 }
 
 div.addEventListener("click", saludar);
